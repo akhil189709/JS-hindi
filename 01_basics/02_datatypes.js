dfa2aaa08f9baca3a4f49c99f1d403d1966631c4
Akhil kumar <106308351+akhil189709@users.noreply.github.com>
2024-08-02 15:44:23 +0000

    datatypes

---
+++ 01_basics/02_datatypes.js
@@ -0,0 +1,30 @@
+"use strict"//treat all the code of javascript as a newer version of 
+
+//alert(3+4)  we are using node not the browser:
+console.log(3 + 3);
+console.log("akhil kumar");
+// code readability should be high::
+
+let name = "akhil";
+let age = 55;
+let isLoggedin = false;
+
+// number => 2 to power 53;
+//bigint  
+//string => " "
+//Boolean => true/ false;
+//null => standalone value;
+//undefined => value is not defined;
+//symbol => unique;
+
+
+console.log(typeof "akhil kumar");
+console.log(typeof age);
+console.log(typeof null);//// type of null is object
+console.log(typeof undefined);//type of undefined is undefined
+
+
+
+
+
+

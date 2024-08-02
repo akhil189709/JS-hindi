11371638aadd0370d2de37a7164aab4df4f29384
Akhil kumar <106308351+akhil189709@users.noreply.github.com>
2024-08-02 17:47:04 +0000

    conversions of datatypes

---
+++ 01_basics/03_conversionsOperations.js
@@ -0,0 +1,33 @@
+let score = true;
+
+console.log(typeof score);
+console.log(typeof(score));
+let valueInNumber = Number(score);
+console.log(typeof valueInNumber);
+console.log(valueInNumber);
+
+
+// "33" => 33
+// "33ad" => NaN
+// true => 1; false => 0;
+ 
+
+let isLoggedIn = "akhil";
+let booleanLoggedIn = Boolean(isLoggedIn);
+console.log(typeof booleanLoggedIn);
+console.log(booleanLoggedIn);
+
+// 1=> true ;
+// 2=> false;
+// "" => false;
+// "akhil" => true;
+
+let someNumber = 33;
+let stringNumber  = String(someNumber);
+console.log(typeof stringNumber);
+console.log(stringNumber);
+
+
+
+
+
--- 01_basics/test.js
+++ 01_basics/test.js
@@ -1,1 +1,23 @@
-console.log("akhil")
+console.log("akhil");
+const accountId = 122322;
+let names = "akhil";
+let accountEmail = "akhil@gmail.com"
+let accountPassword = 342232;
+var marks = 32;
+accountEmail = "afjf@gmail.com"
+accountPassword = 4839843;
+let accountState;
+
+
+// accountId = 343;//not allowed
+console.log(accountId);
+console.log(accountEmail);
+console.log(accountPassword);
+console.table([accountId,accountEmail,accountPassword,marks,names,accountState]);
+
+/*
+prefer not to use var because of issue in block scope:
+*/
+
+
+

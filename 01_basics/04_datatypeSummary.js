d21c639a46c9a40339f75849a26f83398e3c1595
Akhil kumar <106308351+akhil189709@users.noreply.github.com>
2024-08-03 08:49:09 +0000

    datatype summary

---
+++ 01_basics/04_datatypeSummary.js
@@ -0,0 +1,34 @@
+// primitive
+
+//7 types : Numbers, Strings, Boolean, null, undefined,Symbol, BingInt;
+
+
+const score = 100;
+const scoreValue = 100.4;
+
+const islogegedIn = false;
+const outsideTemp = null;
+let userEmail;
+
+const id = Symbol('222');
+const anotherId = Symbol('222');
+console.log(id == anotherId);
+
+
+// Reference type(Non-Primitive)
+
+//Array, objects, Functions
+
+const hereos = ["hero","krrissh","shaktimaan"];
+
+let myObj={
+
+    name:"akhil",
+    age: 22,
+}
+
+ const myFunction = function(){
+    console.log("hello world");
+    
+}
+

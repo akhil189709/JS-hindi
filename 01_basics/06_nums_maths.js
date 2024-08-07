04fe2574004069b8fcc309adaf37af9c262ec253
Akhil kumar <106308351+akhil189709@users.noreply.github.com>
2024-08-07 08:35:19 +0000

    numbers and maths operations

---
+++ 01_basics/06_nums_maths.js
@@ -0,0 +1,12 @@
+console.log(Math.random());/// gives the value between the 0 and 1
+
+console.log(Math.random()*10 + 1);  /// gives the value betweent the 0 and 10;
+
+console.log(Math.floor(Math.random()*10) + 1);/// gives the value between 0 and 9;
+
+const min = 10; 
+const max = 20;
+
+console.log(Math.floor(Math.random() * (max - min + 1)) + min);
+
+
